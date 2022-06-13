740da26a6d1bb7e2a03e54b4edf1df30afabf264
Marcos M. Jiménez <marcos.davia@gmail.com>
2022-06-13 14:00:44 +0200

    Modified programs in web-service-mlflow to use port 9000 and own S3 bucket to make predictions and mlflow server running in EC2 instance (EC2 running mlflow connected to S3 and postgres (RDS)), whether to get registered model in prediction or use as a tracking server in training. Also made Dockerfile to make an image with prediction server which may get the model from S3 or mlflow server.

--- 04-deployment/web-service-mlflow/predict.py
+++ 04-deployment/web-service-mlflow/predict.py
@@ -4,12 +4,32 @@ import pickle
 import mlflow
 from flask import Flask, request, jsonify
 
-RUN_ID = os.getenv('RUN_ID')
+RUN_ID = os.getenv('RUN_ID', '6574d0d7c4b044f585c08de11b3582c4')
 
-logged_model = f's3://mlflow-models-alexey/1/{RUN_ID}/artifacts/model'
-# logged_model = f'runs:/{RUN_ID}/model'
+# Load the model from the tracking server
+
+# MLFLOW_TRACKING_URI = 'http://18.200.196.114:5000'
+# mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+# model = mlflow.pyfunc.load_model(f'runs:/{RUN_ID}/model')
+
+# Note that when generating the model with the random-forest.ipynb, a pipeline is used, so the model logged is the pipeline, which
+# includes the dv. 
+# If we had not done it that way, we would have to download the artifact (dv) with mlflow client and use it. Tedious.
+
+# Instead of using the tracking server to retrieve the model, we use directly the artifact storage, in this case is S3 bucket.
+# Avoiding using the tracking server. 
+
+logged_model = f's3://mlops-bucket-mmjd/2/{RUN_ID}/artifacts/model'
+
+# If we want to use model registry, we need the tracking server:
+# We may pass the connections parameters via env vars, specially when using docker
+# MLFLOW_TRACKING_URI = os.getenv('RUN_ID','http://18.200.196.114:5000')
+MODEL_NAME = os.getenv('MODEL_NAME','green-taxi-duration-RandomForest')
+STAGE = os.getenv('STAGE','production')
+# mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+#logged_model = f'models:/{MODEL_NAME}/{STAGE}'
+
 model = mlflow.pyfunc.load_model(logged_model)
-
 
 def prepare_features(ride):
     features = {}
@@ -35,11 +55,11 @@ def predict_endpoint():
 
     result = {
         'duration': pred,
-        'model_version': RUN_ID
+        'model_version': MODEL_NAME+'-'+STAGE
     }
 
     return jsonify(result)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=9696)
+    app.run(debug=True, host='0.0.0.0', port=9000)
